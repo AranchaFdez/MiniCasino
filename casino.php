b0053d355b1a0a895727fd7aed157ca9a4647e29
AranchaFdez <72246899+AranchaFdez@users.noreply.github.com>
2020-11-17 19:27:53 +0100

    Update casino.php

--- casino.php
+++ casino.php
@@ -8,45 +8,25 @@ if(isset($_COOKIE['visitas'])){
 function generarNumero(){
     return mt_rand(1,100);
 }
+function generarResultado($num) {
+    $isPar="Impar";
+    if($num%2==0) $isPar="Par";
+       return $isPar;
+}
 ?>
 <html>
 <head><title>Casino</title>
-	<style>
-	   body{
-	       margin:2% 30%;
-	   }
-	   div{
-	   border: 1px black solid;
-	   background: #8f9ed1;
-	   padding:0px 10px 10px;
-	   width:50%;
-	   
-	   }
-	   #botones{
-	       margin-left:20%;
-	       border:none;
-	   }
-	   #botones > input{
-	       margin:2%;
-	       width: 40%;
-	   }
-	    #botones > input:last-child{
-	       	       width: 85%;
-
-	    }
-	</style>
-
 </head>
 <body>
 <div>
     <?php 
-    
     //si no esta declarada la sesion muestra este formulario
     //Mensaje de bienvenida y la cantidad con la que quiere jugar 
     if(!isset($_SESSION['cantidadInicio'])){
-    	echo "<h1>Bienvenido!!!</h1>";
+    	echo "<h1>BIENVENIDO AL CASINO!!!</h1>";
+    	echo "Nº Visitas:  ".$visitas;
     	echo"<form method='post' >";
-    	echo"Saldo inicial <input type='number' name='cantidadInicial'>";
+    	echo"Saldo inicial :<input type='number' name='cantidadInicial'>";
     	echo"</form>";
     	//si la cantidad es superior a 0 y esta declarada  refresca la pag
     	if(!empty($_POST['cantidadInicial'])&& isset($_POST['cantidadInicial'])&&$_POST['cantidadInicial']>0){
@@ -59,51 +39,47 @@ function generarNumero(){
     }else{
         //si esta declaraca la session volcamos en una variable la sesion y mostramos otro formulario
         $cantidad=$_SESSION['cantidadInicio'];
-        echo "<h1>Tu Saldo :".$cantidad."</h1>";
+       
+        echo "<h3>Tu Saldo es : ".$cantidad."</h3>";
         echo"<form method='post' >";
         echo"Tu apuesta es: <input type='number' name='apuesta'><br>";
-        echo"<div id='botones'><input type='submit' value='Par' name='par'><input type='submit' value='Impar' name='impar'><br>";
-        echo "<input type='submit' value='Salir 'name='salir'></div>";
+        echo"Tipo de apuesta :  Par<input type='radio' value='Par' name='par'>  Impar<input type='radio' value='Impar' name='impar'><br>";
+        echo "<input type='submit' value='Apostar Cantidad' name='enviar'><input type='submit' value='Abandonar Casino 'name='salir'>";
         echo"</form>";
     }
     if(isset($_POST['salir'])){
-        echo "<h3>Hasta la proxima</h3>";
+        echo "<h3>Su resultado final es de ".$_SESSION['cantidadInicio']."€ </h3>";
         session_destroy();
         header("refresh: 3;");
         exit();
     }
     if(!empty($_POST['apuesta'])&& isset($_POST['apuesta'])&&$_POST['apuesta']>0){
         if($_POST['apuesta']<=$_SESSION['cantidadInicio']){
-        $num=generarNumero();
+        $num=generarNumero();  
         if(isset($_POST['par'])|| isset($_POST['impar'])){
             if(($num%2==0)==isset($_POST['par'])){
-                $resultado="Has ganado";
+                $resultado="Has ganado!!";
                 $_SESSION['cantidadInicio']+=$_POST['apuesta'];
             }elseif(!($num%2==0)==isset($_POST['impar'])){
-                $resultado="Has ganado";
+                $resultado="Has ganado!!";
                 $_SESSION['cantidadInicio']+=$_POST['apuesta'];
             }else{
-                $resultado="Has perdido";
+                $resultado="Has perdido!!";
                 $_SESSION['cantidadInicio']-=$_POST['apuesta'];
             }
-            header("refresh: 2;"); // refresca la página
-          
-            echo "<h1>".$resultado."</h1>";
+            header("refresh: 3;"); // refresca la página
+            echo "El resultado de la apuesta es :".generarResultado($num);
+            echo "<h2>".$resultado."</h2>";
             }
         }else{
             echo "<h3 style='color: red;'>No puede apostar mas del saldo</h3>";
-        } 
-        
-        
+        }    
         if( $_SESSION['cantidadInicio']==0  ){
             echo "<h3 style='color: red;'>No puede seguir jugando.No tiene saldo</h3>";
             session_destroy();
             exit();
-            
         }
     } 
-    
-    echo "Nº Visitas:  ".$visitas;
     ?>
 </div>
 </body>
